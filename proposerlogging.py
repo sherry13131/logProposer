c50c29c418d97e20530a721eef3c76aa65c64f99
Sherry Ma <sherry.hyma@gmail.com>
2018-08-16 04:50:24 +0000

    fix some bugs

--- proposerlogging.py
+++ proposerlogging.py
@@ -2,7 +2,6 @@
 from __future__ import division
 from systemd import journal
 import re
-from pprint import pprint
 # import pymongo
 # from pymongo import MongoClient
 import json
@@ -18,6 +17,7 @@ from requests_futures.sessions import FuturesSession
     # get the proposer from dump_consense_state,
     # keep track on the log, log down absent validators, calculate the timeout_commit
 
+fo = open("Proposerlog.log", "a")
 
 j = journal.Reader()
 j.this_boot()
@@ -25,11 +25,11 @@ j.seek_tail()
 # Important! - Discard old journal entries
 j.get_previous()
 j.log_level(journal.LOG_INFO)
-j.add_match(_SYSTEMD_UNIT="forboled.service")
+j.add_match(_SYSTEMD_UNIT="gaiad.service")
 
 #start = time.time()
 
-preHeight = 0
+currHeight = 0
 
 # check the current height when found this line in log
 try:
@@ -39,20 +39,23 @@ try:
             getProposer = re.search(r'/.*I\[[0-9]{2}-[0-9]{2}\|([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})] Received proposal\s*module=consensus proposal="Proposal{([0-9]{1,})\/0.*/', str(entry))
 
             # get the proposer of that round
-            temp = re.search(r'/.*I\[[0-9]{2}-[0-9]{2}\|([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})] enterPropose: (Not our turn to propose)?(Our turn to propose)?\ *module=consensus height=([0-9]{1,}) round=[0-9]{1,} proposer=([0-9A-F]{40}).*/', str(entry))
+#            temp = re.search(r'/.*I\[[0-9]{2}-[0-9]{2}\|([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})] enterPropose: (Not our turn to propose)?(Our turn to propose)?\ *module=consensus height=([0-9]{1,}) round=[0-9]{1,} proposer=([0-9A-F]{40}).*/', str(entry))
 
             # check Absent validator
             matchAbsent = re.search(r'/.*I\[([0-9]{2}-[0-9]{2})\|([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})\] Absent validator ([0-9A-F]{40}) at height ([0-9]{1,}), ([0-9]{1,}) signed, threshold ([0-9]{1,}).*/' , str(entry))
 
             # check timeout_commit of proposer of that round
-            getCommitTimeout = re.search(r'/.*I\[[0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}\] Timed out\s*module=consensus dur=([0-9]{1,}.[0-9]{1,})s height=([0-9]{1,}) round=[0-9]{1,} step=RoundStepNewHeight.*/', str(entry))
+            getCommitTimeout = re.search(r'/.*Timed out\s*module=consensus dur=([0-9]{1,}.[0-9]{1,}s) height=([0-9]{1,}) round=[0-9]{1,} step=RoundStepNewHeight.*/', str(entry))
 
             msg = entry["MESSAGE"]
+            if ("Timed out" in msg and "step=RoundStepNewHeight" in msg):
+                fo.write(msg + '\n')
+#            print(msg)
 
             if (getProposer):
-                startTime = checkHeight.group(1)
-                height = checkHeight.group(2)
-                if (height != preHeight):
+                startTime = getProposer.group(1)
+                h = getProposer.group(2)
+                if (h > currHeight):
                     # get proposer
                     #print("getting proposer")
                     session = FuturesSession()
@@ -63,27 +66,28 @@ try:
                     jsonstr = unijson.encode("ascii", "replace")
                     myjson = json.loads(jsonstr)
                     proposerAddr = myjson['result']['round_state']['validators']['proposer']['address']
-                    print('Height: ' + height + ', Proposer: ' + proposerAddr)
+                    fo.write('Height: ' + h + ', Proposer: ' + proposerAddr+ '\n')
 
-                    preheight = height
+                    currHeight = h
+
+            elif (getCommitTimeout):
+                print("hi")
+                timeout = getCommitTimeout.group(1)
+                h = getCommitTimeout.group(2)
+#                if (h == currHeight):
+                fo.write("Height: " + h + ", Timeout reached: " + timeout + '\n')
+#                else:
+#                    print("Time not out")
 
             elif (matchAbsent):
                 valaddr = matchAbsent.group(3)
                 h = matchAbsent.group(4)
-                if (h == height):
-                    print("Absent validator: " + valaddr)
-                else:
-                    print("It should not appear this line...")
-
-            elif (getCommitTimeout):
-                timeout = getCommitTimeout.group(1)
-                h = getCommitTimeout.group(2)
-                if (height == h):
-                    print("Timeout reached: " + timeout)
-                else:
-                    print("Time not out")
-
+#                if (h == currHeight):
+                fo.write("Height: " + h + ", Absent validator: " + valaddr + '\n')
+#                else:
+#                    print("It should not appear this line...")
 
 except KeyboardInterrupt:
+    fo.close()
     print("Thanks for using")
 
